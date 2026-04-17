a52761fcf6c63ef7eaa219dd6de51e5ac5d6809a
ajithhtija <87263974+ajithhtija@users.noreply.github.com>
2026-04-17 14:56:18 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False]*n
+        j = n-2
+        dp[n-1] = True
+        while j>=0:
+            k = 1
+            while k<=nums[j] and k+j<n:
+                dp[j] = dp[j] or dp[k+j]
+                k+=1
+                if dp[j]==True:
+                    break
+            j-=1
+        return dp[0]
+
+
+        
